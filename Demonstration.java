f441011bec7a6e7e3a613389706ffa94b459bdda
austin-li <austinmingli@gmail.com>
2022-02-03 18:25:32 -0800

    add failing JUnit test

---
+++ Demonstration.java
@@ -0,0 +1,5 @@
+public class Demonstration {
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ DemonstrationTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DemonstrationTest {
+	@Test
+	public void testAdd() {
+		assertEquals(2, Demonstration.add(1, 2));
+	}
+}
